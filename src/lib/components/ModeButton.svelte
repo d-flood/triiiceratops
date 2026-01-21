404196d57fb4ef6d5e7eb48396db6d495d69e045
Ryan Muther <ryan_muther@fas.harvard.edu>
2026-01-21 15:55:40 -0500

    Added new properties to config in header

--- src/lib/components/ModeButton.svelte
+++ src/lib/components/ModeButton.svelte
@@ -10,17 +10,18 @@
         viewerState.toggleTwoPageMode();
     }
 
-    const modeTooltip = viewerState.twoPageMode
-            ? m.single_page_mode()
-            : m.two_page_mode();
+    const twoPageMode: boolean =  $derived(viewerState.twoPageMode !== false);
+
+    const modeTooltip = $derived(twoPageMode ? m.toggle_single_page_mode() : m.toggle_two_page_mode());
 </script>
+
 {#if viewerState.showModeToggle}
     <div
         class="absolute left-6 top-6 z-50 pointer-events-auto flex flex-col items-start transition-all duration-300"
     >
         <button
             class="bg-white dark:bg-gray-800 p-2 rounded-md shadow-md hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-blue-500"
-            on:click={toggleMode}
+            onclick={toggleMode}
             aria-label={modeTooltip}
             title={modeTooltip}>
             {#if viewerState.twoPageMode}
